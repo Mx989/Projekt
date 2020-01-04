1c616eaec5df87b8ad118bf88166029e440796b3
Dariusz Kubacki <jack.qeeze@gmail.com>
2020-01-04 16:49:40 +0100

    Added comments, hourly forecast, links to AccuWeatherApi

--- Projekt/WpfApp/AccuWeatherApi.cs
+++ Projekt/WpfApp/AccuWeatherApi.cs
@@ -6,25 +6,32 @@ using System.Threading.Tasks;
 
 namespace WpfApp
 {
-    //search via "City search"
-    class CistySerch
+    //search via "Text search" (https://developer.accuweather.com/accuweather-locations-api/apis/get/locations/v1/cities/search)
+    class CitySearch
     {
+        //location key used in forecast search
         public string Key { get; set; }
+        //City name
         public string LocalizedName { get; set; }
-        public SupplementalAdminAreas SupplementalAdminAreas { get; set; }
+        public List<SupplementalAdminAreas> SupplementalAdminAreas { get; set; }
     }
     class SupplementalAdminAreas
     {
+        //1-x podziały administracyjne, w polsce to przeważnie 2- powiat 3-gmina
+        public int Level { get; set; }
         public string LocalizedName { get; set; }
     }
+    //1-Day DailyForecast(https://developer.accuweather.com/accuweather-forecast-api/apis/get/forecasts/v1/daily/1day/%7BlocationKey%7D)
     class Forecast
     {
         public Headline Headline { get; set; }
+        //dziena prognoza
         public List<DailyForecasts> DailyForecasts { get; set; }
 
     }
     class Headline
     {
+        //nagłówek prognozy
         public string Text { get; set; }
         public string Link { get; set; }
 
@@ -53,8 +60,9 @@ namespace WpfApp
     }
     class Day
     {
-        // we have to dowload icon list from https://developer.accuweather.com/weather-icons
+        // we have to dowload icon list from https://developer.accuweather.com/weather-icons, for eg. sunny day ico
         public int Icon { get; set; }
+        // the name of the icon for eg. sunny day
         public string IconPhrase { get; set; }
         public bool HasPrecipitation { get; set; }
         //PrecipitationIntensity&PrecipitationIntensity can be not includet in case of HasPrecipitation==false
@@ -63,12 +71,31 @@ namespace WpfApp
     }
     class Night
     {
-        // we have to dowload icon list from https://developer.accuweather.com/weather-icons
+        // we have to dowload icon list from https://developer.accuweather.com/weather-icons, for eg. sunny day ico
         public int Icon { get; set; }
+        // the name of the icon for eg. sunny day
         public string IconPhrase { get; set; }
         public bool HasPrecipitation { get; set; }
         //PrecipitationIntensity&PrecipitationIntensity can be not includet in case of HasPrecipitation==false
         public string PrecipitationType { get; set; }
         public string PrecipitationIntensity { get; set; }
+    }
+    //12 hours hourly forecast- this is free(https://developer.accuweather.com/accuweather-forecast-api/apis/get/forecasts/v1/hourly/12hour/%7BlocationKey%7D)
+    class HourlyForecast
+    {
+        public string DateTime { get; set; }
+        public int WeatherIcon { get; set; }
+        public string IconPhrase { get; set; }
+        public string HasPrecipitation { get; set; }
+        public string IsDaylight { get; set; }
+        public string PrecipitationType { get; set; }
+        public string PrecipitationIntensity { get; set; }
+        public HourlyTemperature Temperature { get; set; }
+        public string PrecipitationProbability { get; set; }
+        public string link { get; set; }
+    }
+    class HourlyTemperature
+    {
+        public string Value { get; set; }
     }
 }
