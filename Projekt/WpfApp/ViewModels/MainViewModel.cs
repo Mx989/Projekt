0f41e2a8a910e6b0b396047d03c792c439adf26e
Mx989 <>
2020-01-18 00:10:40 +0100

    Add structures, part 1

---
+++ Projekt/WpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp.Views;
+
+namespace WpfApp.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private ICommand _goToNewsCommand;
+
+        private object _currentNewsView;
+        private object _newsViewA;
+        private object _newsViewB;
+
+        public MainViewModel()
+        {
+            _newsViewA = new NewsViewA();
+            _newsViewB = new NewsViewB();
+
+            CurrentNewsView = _newsViewA;
+        }
+
+        public object CurrentNewsView
+        {
+            get { return _currentNewsView; }
+            set
+            {
+                _currentNewsView = value;
+                OnPropertyChanged("Current View");
+            }
+        }
+
+       //commands TODO
+
+    }
+}
---
+++ Projekt/WpfApp/ViewModels/NewsViewAViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.ViewModels
+{
+    class NewsViewAViewModel
+    {
+        public string TheImage //TEMPORARY
+        {
+            get { return "http://images.pexels.com/photos/744667/pexels-photo-744667.jpeg"; }
+        }
+    }
+}
