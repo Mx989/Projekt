c003b7f4bd84d4542c3f77ba6b32a8ce3724111c
Mx989 <>
2020-01-18 19:04:44 +0100

    Update weather - part 1

--- Projekt/WpfApp/ViewModels/WeatherViewAViewModel.cs
+++ Projekt/WpfApp/ViewModels/WeatherViewAViewModel.cs
@@ -10,16 +10,38 @@ namespace WpfApp.ViewModels
 {
     class WeatherViewAViewModel
     {
+        public string Date
+        {
+            get { return DateTime.Now.ToString(); }
+        }
+
+        public string Day
+        {
+            get { return DateTime.Now.DayOfWeek.ToString(); }
+        }
         public string CurrentWeatherImage//TEMPORARY
         {
             get { return "https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-sunny-512.png"; }
         }
 
+        private string _currentTemperature = "30°C";
+
+        public string CurrentTemperature
+        {
+            get { return _currentTemperature; }
+        }
         public string NextHourWeatherImage//TEMPORARY
         {
             get { return "https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-sunny-512.png"; }
         }
 
+        private string _nextHourTemperature = "24°C";
+        
+        public string NextHourTemperature
+        {
+            get { return _nextHourTemperature; }
+        }
+        
         public WeatherViewAViewModel()
         {
 
