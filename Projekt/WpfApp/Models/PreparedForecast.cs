e5e5f2aee1056f43fe1f993f659d8ec353633d63
Mx989 <56219326+Mx989@users.noreply.github.com>
2020-01-25 11:11:46 +0100

    Update layout - part 1

--- Projekt/WpfApp/Models/PreparedForecast.cs
+++ Projekt/WpfApp/Models/PreparedForecast.cs
@@ -9,11 +9,26 @@ namespace WpfApp.Models
     public class PreparedForecast
     {
         public DateTime Date { get; }
+        public string FullHour
+        {
+            get
+            {
+                if (Date.Hour.ToString().Length == 1) return Date.Hour.ToString() + ":00  ";
+                return Date.Hour.ToString() + ":00";
+            }
+        }
         public string ImgUrl { get; }
         public string Temperature { get; }
         public string PrecipitationType { get; }
         public string PrecipitationIntensity { get; }
         public string PrecipitationProbability { get; }
+        public string Precipitation
+        {
+            get
+            {
+                return PrecipitationIntensity + " " + PrecipitationType;
+            }
+        }
 
         public PreparedForecast(DateTime dateTime, string imgUrl, string temperature, string precipitationType, string precipitationIntensity, string precipitationProbability)
         {
